5cabbd09209a154fcf979bf7c8b63da2207a9c55
Thanassis Boulis <boulis@gmail.com>
2010-03-25 05:22:02 +0000

    Updated Resource Manager to crop clock drifts beyond +/- 3sigmas

--- Castalia/src/Node/Resource_Manager/ResourceGenericManager.cc
+++ Castalia/src/Node/Resource_Manager/ResourceGenericManager.cc
@@ -1,17 +1,13 @@
-//***************************************************************************************
-//*  Copyright: National ICT Australia,  2007, 2008, 2009				*
-//*  Developed at the Networks and Pervasive Computing program				*
-//*  Author(s): Athanassios Boulis, Dimosthenis Pediaditakis, Yuri Tselishchev		*
-//*  This file is distributed under the terms in the attached LICENSE file.		*
-//*  If you do not find this file, copies can be found by writing to:			*
-//*											*
-//*      NICTA, Locked Bag 9013, Alexandria, NSW 1435, Australia			*
-//*      Attention:  License Inquiry.							*
-//*											*
-//***************************************************************************************
-
- 
-
+/********************************************************************************
+ *  Copyright: National ICT Australia,  2007 - 2010                             *
+ *  Developed at the ATP lab, Networked Systems theme                           *
+ *  Author(s): Athanassios Boulis, Dimosthenis Pediaditakis, Yuri Tselishchev   *
+ *  This file is distributed under the terms in the attached LICENSE file.      *
+ *  If you do not find this file, copies can be found by writing to:            *
+ *                                                                              *
+ *      NICTA, Locked Bag 9013, Alexandria, NSW 1435, Australia                 *
+ *      Attention:  License Inquiry.                                            *
+ ********************************************************************************/
 
 #include "ResourceGenericManager.h"
 
@@ -19,114 +15,110 @@ Define_Module(ResourceGenericManager);
 
 void ResourceGenericManager::initialize() {
 	sigmaCPUClockDrift = par("sigmaCPUClockDrift");
-	cpuClockDrift = normal(0, sigmaCPUClockDrift);  //using the "0" rng generator of the ResourceGenericManager module 
+	//using the "0" rng generator of the ResourceGenericManager module
+	cpuClockDrift = normal(0, sigmaCPUClockDrift);
+	/* Crop any values beyond +/- 3 sigmas. Some protocols (e.g., MAC) rely on
+	 * bounded cpuClockDrift. Although the bounds are conservative (usually 3sigmas),
+	 * if you instantiate thousands of nodes (in multiple runs) we will get a
+	 * couple of nodes that will be beyond this bound. Limiting/Croping the drift
+	 * is actually realistic, since usually there is some kind of quality
+	 * control on quartz crystals or the boards that use them (sensor node)
+	 */
+	if (cpuClockDrift > 3*sigmaCPUClockDrift) cpuClockDrift = 3*sigmaCPUClockDrift;
+	if (cpuClockDrift < -3*sigmaCPUClockDrift) cpuClockDrift = -3*sigmaCPUClockDrift;
+
 	initialEnergy = par("initialEnergy");
 	ramSize = par("ramSize");
 	baselineNodePower = par("baselineNodePower");
 	periodicEnergyCalculationInterval = (double)par("periodicEnergyCalculationInterval")/1000;
-	
+
 	if (baselineNodePower < 0 || periodicEnergyCalculationInterval < 0) opp_error("Illegal values for baselineNodePower and/or periodicEnergyCalculationInterval in resource manager module");
-	
+
 	currentNodePower = baselineNodePower;
 	remainingEnergy = initialEnergy;
 	timeOfLastCalculation = simTime();
 	totalRamData = 0;
-	
+
 	energyMsg = new cMessage("Periodic energy calculation message", TIMER_SERVICE);
 	scheduleAt(simTime() + periodicEnergyCalculationInterval, energyMsg);
 }
 
 void ResourceGenericManager::calculateEnergySpent() {
-    simtime_t timePassed = simTime() - timeOfLastCalculation;
-    trace() << "energy consumed in the last " << timePassed << "s is " << (timePassed * currentNodePower);
-    consumeEnergy(SIMTIME_DBL(timePassed * currentNodePower / 1000.0));
-    timeOfLastCalculation = simTime();
-    if (remainingEnergy > 0) {
-	cancelEvent(energyMsg);
-	scheduleAt(simTime() + periodicEnergyCalculationInterval, energyMsg);
-    }
+	simtime_t timePassed = simTime() - timeOfLastCalculation;
+	trace() << "energy consumed in the last " << timePassed << "s is " << (timePassed * currentNodePower);
+	consumeEnergy(SIMTIME_DBL(timePassed * currentNodePower / 1000.0));
+	timeOfLastCalculation = simTime();
+	if (remainingEnergy > 0) {
+		cancelEvent(energyMsg);
+		scheduleAt(simTime() + periodicEnergyCalculationInterval, energyMsg);
+	}
 }
 
-void ResourceGenericManager::handleMessage(cMessage *msg)
-{
-    //The ResourceGenericManager module is not connected with other modules.
-    //They use instead its public methods.
-    //The only possible message is periodic energy consumption. There is no
-    //message object associated to that message kind
 
-    switch (msg->getKind()) {
-	case TIMER_SERVICE: {
-	    calculateEnergySpent();
-	    return;
-	}
-	
-	case RESOURCE_MANAGER_DRAW_POWER: {
-	    ResourceManagerMessage *resMsg = check_and_cast<ResourceManagerMessage*>(msg);
-	    int id = resMsg->getSenderModuleId();
-	    double oldPower = storedPowerConsumptions[id];
-	    trace() << "New power consumption, id = " << id << ", oldPower = " << currentNodePower << ", newPower = " << currentNodePower - oldPower + resMsg->getPowerConsumed();
-	    calculateEnergySpent();
-	    currentNodePower = currentNodePower - oldPower + resMsg->getPowerConsumed();
-	    storedPowerConsumptions[id] = resMsg->getPowerConsumed();
-	    break;
-	}
+/* The ResourceGenericManager module is not connected with other modules. They use instead its public methods.
+ * The only possible message is periodic energy consumption. There is no message object associated to that message kind.
+ */
+void ResourceGenericManager::handleMessage(cMessage *msg) {
+	switch (msg->getKind()) {
+		case TIMER_SERVICE: {
+			calculateEnergySpent();
+			return;
+		}
 
-	default: {
-	    opp_error("ERROR: Unexpected message received by resource manager: %s",msg->getKind());
-	}
+		case RESOURCE_MANAGER_DRAW_POWER: {
+			ResourceManagerMessage *resMsg = check_and_cast<ResourceManagerMessage*>(msg);
+			int id = resMsg->getSenderModuleId();
+			double oldPower = storedPowerConsumptions[id];
+			trace() << "New power consumption, id = " << id << ", oldPower = " << currentNodePower << ", newPower = " << currentNodePower - oldPower + resMsg->getPowerConsumed();
+			calculateEnergySpent();
+			currentNodePower = currentNodePower - oldPower + resMsg->getPowerConsumed();
+			storedPowerConsumptions[id] = resMsg->getPowerConsumed();
+			break;
+		}
+
+		default: {
+			opp_error("ERROR: Unexpected message received by resource manager: %s",msg->getKind());
+		}
     }
     delete msg;
 }
 
-
-void ResourceGenericManager::finishSpecific()
-{
+void ResourceGenericManager::finishSpecific() {
 	calculateEnergySpent();
 	declareOutput("Consumed Energy");
 	collectOutput("Consumed Energy","",getSpentEnergy());
-	// DO NOT ADD HERE ANY CODE FOR RECORDING SCALARS OR VECTORS
-	// SOME MODULES MAY CONSUME ADDITIONAL ENERGY OR OCCUPY EXTRA RESOURCES
-	// AFTER THE END OF THIS FINISH() 
 }
 
-double ResourceGenericManager::getSpentEnergy(void)
-{
+double ResourceGenericManager::getSpentEnergy(void) {
 	Enter_Method("getSpentEnergy()");
-	
 	return (initialEnergy - remainingEnergy);
 }
 
 
-double ResourceGenericManager::getCPUClockDrift(void)
-{
+double ResourceGenericManager::getCPUClockDrift(void) {
 	Enter_Method("getCPUClockDrift(void)");
 	return (1.0f+cpuClockDrift);
 }
 
 
-void ResourceGenericManager::consumeEnergy(double amount)
-{
+void ResourceGenericManager::consumeEnergy(double amount) {
 	Enter_Method("consumeEnergy(double amount)");
-	
-	if(remainingEnergy < amount)
-	{
-	    remainingEnergy = 0;
-	    send(new cMessage("Destroy node message", DESTROY_NODE), "toSensorDevManager");
-	    send(new cMessage("Destroy node message", DESTROY_NODE), "toApplication");
-	    send(new cMessage("Destroy node message", DESTROY_NODE), "toNetwork");
-	    send(new cMessage("Destroy node message", DESTROY_NODE), "toMac");
-	    send(new cMessage("Destroy node message", DESTROY_NODE), "toRadio");
+
+	if(remainingEnergy < amount) {
+		remainingEnergy = 0;
+		send(new cMessage("Destroy node message", DESTROY_NODE), "toSensorDevManager");
+		send(new cMessage("Destroy node message", DESTROY_NODE), "toApplication");
+		send(new cMessage("Destroy node message", DESTROY_NODE), "toNetwork");
+		send(new cMessage("Destroy node message", DESTROY_NODE), "toMac");
+		send(new cMessage("Destroy node message", DESTROY_NODE), "toRadio");
 	}
-	else
-	    remainingEnergy -= amount;
+	else remainingEnergy -= amount;
 }
 
 
-
-void ResourceGenericManager::destroyNode(void)
-{
+void ResourceGenericManager::destroyNode(void) {
 	Enter_Method("destroyNode(void)");
-	
+
 	send(new cMessage("Destroy node message", DESTROY_NODE), "toSensorDevManager");
 	send(new cMessage("Destroy node message", DESTROY_NODE), "toApplication");
 	send(new cMessage("Destroy node message", DESTROY_NODE), "toNetwork");
@@ -135,29 +127,21 @@ void ResourceGenericManager::destroyNode(void)
 }
 
 
-int ResourceGenericManager::RamStore(int numBytes)
-{
+int ResourceGenericManager::RamStore(int numBytes) {
 	Enter_Method("RamStore(int numBytes)");
-	
+
 	int ramHasSpace = ((totalRamData+numBytes) <= ramSize)?1:0;
-	
-	if(!ramHasSpace)
-	{
+	if(!ramHasSpace) {
 		trace() << "\n[Resource Manager] t= " << simTime() << ": WARNING: Data not stored to Ram. Not enough space to store them.";
 		return 0;
 	}
-	else
-		totalRamData += numBytes;
-
+	else totalRamData += numBytes;
 	return 1;
 }
 
 
-void ResourceGenericManager::RamFree(int numBytes)
-{
+void ResourceGenericManager::RamFree(int numBytes) {
 	Enter_Method("RamFree(int numBytes)");
-	
 	totalRamData -= numBytes;
-	
 	totalRamData = (totalRamData < 0)?0:totalRamData;
 }

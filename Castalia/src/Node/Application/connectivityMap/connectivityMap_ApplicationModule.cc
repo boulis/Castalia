aecbd2b440255cb0689257fbd782843104eea702
Thanassis Boulis <boulis@gmail.com>
2010-04-23 06:10:03 +0000

    Minor updates for valueProp and connectivityMap apps

--- Castalia/src/Node/Application/connectivityMap/connectivityMap_ApplicationModule.cc
+++ Castalia/src/Node/Application/connectivityMap/connectivityMap_ApplicationModule.cc
@@ -1,89 +1,82 @@
-//***************************************************************************************
-//*  Copyright: National ICT Australia,  2007, 2008, 2009, 2010				*
-//*  Developed at the Networks and Pervasive Computing program				*
-//*  Author(s): Athanassios Boulis, Dimosthenis Pediaditakis, Yuriy Tselishchev		*
-//*  This file is distributed under the terms in the attached LICENSE file.		*
-//*  If you do not find this file, copies can be found by writing to:			*
-//*											*
-//*      NICTA, Locked Bag 9013, Alexandria, NSW 1435, Australia			*
-//*      Attention:  License Inquiry.							*
-//*											*
-//***************************************************************************************
-
+/********************************************************************************
+ *  Copyright: National ICT Australia,  2007 - 2010                             *
+ *  Developed at the ATP lab, Networked Systems theme                           *
+ *  Author(s): Athanassios Boulis, Dimosthenis Pediaditakis, Yuri Tselishchev   *
+ *  This file is distributed under the terms in the attached LICENSE file.      *
+ *  If you do not find this file, copies can be found by writing to:            *
+ *                                                                              *
+ *      NICTA, Locked Bag 9013, Alexandria, NSW 1435, Australia                 *
+ *      Attention:  License Inquiry.                                            *
+ ********************************************************************************/
 
 #include "connectivityMap_ApplicationModule.h"
 
 Define_Module(connectivityMap_ApplicationModule);
 
 void connectivityMap_ApplicationModule::startup() {
-    packetSpacing = (double)par("packetSpacing")/1000.0;
-    packetsPerNode = par("packetsPerNode");
-    packetSize = par("packetSize");
-    
-    neighborTable.clear();
-    packetsSent = 0;
-    totalSNnodes = getParentModule()->getParentModule()->par("numNodes");
+	packetSpacing = (double)par("packetSpacing")/1000.0;
+	packetsPerNode = par("packetsPerNode");
+	packetSize = par("packetSize");
 
-    txInterval_perNode = packetsPerNode * packetSpacing;
-    txInterval_total = (txInterval_perNode * totalSNnodes);
+	neighborTable.clear();
+	packetsSent = 0;
+	totalSNnodes = getParentModule()->getParentModule()->par("numNodes");
 
-    if (strtod(ev.getConfig()->getConfigValue("sim-time-limit"),NULL) < txInterval_total) {
-	trace() << "ERROR: Total sim time should be at least = " << txInterval_total;
-	opp_error("\nError: simulation time not large enough for the conectivity map application\n");
-    }
+	txInterval_perNode = packetsPerNode * packetSpacing;
+	txInterval_total = (txInterval_perNode * totalSNnodes);
 
-    double startTxTime = txInterval_perNode * self;
-    setTimer(SEND_PACKET,startTxTime);
+	if (strtod(ev.getConfig()->getConfigValue("sim-time-limit"),NULL) < txInterval_total) {
+		trace() << "ERROR: Total sim time should be at least = " << txInterval_total;
+		opp_error("\nError: simulation time not large enough for the conectivity map application\n");
+	}
+
+	double startTxTime = txInterval_perNode * self;
+	setTimer(SEND_PACKET,startTxTime);
 }
 
 void connectivityMap_ApplicationModule::fromNetworkLayer(ApplicationGenericDataPacket* rcvPacket, const char * source, double rssi, double lqi) {
-    // double theData = rcvPacket->getData(); 
-    // data should always be 0, but we wont check
-    updateNeighborTable(atoi(source),rcvPacket->getSequenceNumber());
+	updateNeighborTable(atoi(source),rcvPacket->getSequenceNumber());
 }
 
 void connectivityMap_ApplicationModule::timerFiredCallback(int timerIndex) {
-    switch (timerIndex) {
-	case SEND_PACKET: {
-	    if (packetsSent >= packetsPerNode) break;
-	    toNetworkLayer(createGenericDataPacket(0.0,packetsSent,packetSize),BROADCAST_NETWORK_ADDRESS);
-	    packetsSent++;
-	    setTimer(SEND_PACKET,packetSpacing);
-	    break;
+	switch (timerIndex) {
+		case SEND_PACKET: {
+			if (packetsSent >= packetsPerNode) break;
+			toNetworkLayer(createGenericDataPacket(0.0,packetsSent,packetSize),BROADCAST_NETWORK_ADDRESS);
+			packetsSent++;
+			setTimer(SEND_PACKET,packetSpacing);
+			break;
+		}
 	}
-    }
 }
 
 void connectivityMap_ApplicationModule::finishSpecific() {
-    EV << "\n\n** Node [" << self << "] received from:\n";
-    for(int i=0; i < (int)neighborTable.size(); i++) {
-	declareOutput("Packets received",neighborTable[i].id);
-	collectOutput("Packets received",neighborTable[i].id,"Success",neighborTable[i].receivedPackets);
-	collectOutput("Packets received",neighborTable[i].id,"Fail",packetsPerNode-neighborTable[i].receivedPackets);
-	EV << "[" << self << "<--" << neighborTable[i].id << "]\t -->\t " << neighborTable[i].receivedPackets << "\t out of \t" << packetsPerNode << "\n";
-    }
+	for(int i=0; i < (int)neighborTable.size(); i++) {
+		declareOutput("Packets received",neighborTable[i].id);
+		collectOutput("Packets received",neighborTable[i].id,"Success",neighborTable[i].receivedPackets);
+	}
 }
 
-void connectivityMap_ApplicationModule::updateNeighborTable(int nodeID, int theSN) {
-    int i=0, pos = -1;
-    int tblSize = (int)neighborTable.size();
+void connectivityMap_ApplicationModule::updateNeighborTable(int nodeID, int serialNum) {
+	int i=0, pos = -1;
+	int tblSize = (int)neighborTable.size();
 
-    for(i=0; i < tblSize; i++)
-	if(neighborTable[i].id == nodeID) pos = i;
+	for(i=0; i < tblSize; i++)
+		if(neighborTable[i].id == nodeID) pos = i;
 
-    if(pos == -1) {
-	neighborRecord newRec;
-	newRec.id = nodeID;
-	newRec.timesRx = 1;
+		if(pos == -1) {
+			neighborRecord newRec;
+			newRec.id = nodeID;
+			newRec.timesRx = 1;
 
-	if( (theSN >= 0) && (theSN < packetsPerNode) )
-	    newRec.receivedPackets = 1;
+			if( (serialNum >= 0) && (serialNum < packetsPerNode) )
+				newRec.receivedPackets = 1;
 
-	neighborTable.push_back(newRec);
-    } else {
-	neighborTable[pos].timesRx++;
+			neighborTable.push_back(newRec);
+		} else {
+			neighborTable[pos].timesRx++;
 
-	if( (theSN >= 0) && (theSN < packetsPerNode) )
-	    neighborTable[pos].receivedPackets++;
-    }
+			if( (serialNum >= 0) && (serialNum < packetsPerNode) )
+				neighborTable[pos].receivedPackets++;
+	}
 }
--- Castalia/src/Node/Application/valuePropagation/valuePropagation_ApplicationModule.cc
+++ Castalia/src/Node/Application/valuePropagation/valuePropagation_ApplicationModule.cc
@@ -1,72 +1,64 @@
-//***************************************************************************************
-//*  Copyright: National ICT Australia,  2007, 2008, 2009, 2010				*
-//*  Developed at the Networks and Pervasive Computing program				*
-//*  Author(s): Athanassios Boulis, Dimosthenis Pediaditakis, Yuriy Tselishchev		*
-//*  This file is distributed under the terms in the attached LICENSE file.		*
-//*  If you do not find this file, copies can be found by writing to:			*
-//*											*
-//*      NICTA, Locked Bag 9013, Alexandria, NSW 1435, Australia			*
-//*      Attention:  License Inquiry.							*
-//*											*
-//***************************************************************************************
+/********************************************************************************
+ *  Copyright: National ICT Australia,  2007 - 2010                             *
+ *  Developed at the ATP lab, Networked Systems theme                           *
+ *  Author(s): Athanassios Boulis, Dimosthenis Pediaditakis, Yuri Tselishchev   *
+ *  This file is distributed under the terms in the attached LICENSE file.      *
+ *  If you do not find this file, copies can be found by writing to:            *
+ *                                                                              *
+ *      NICTA, Locked Bag 9013, Alexandria, NSW 1435, Australia                 *
+ *      Attention:  License Inquiry.                                            *
+ ********************************************************************************/
 
 #include "valuePropagation_ApplicationModule.h"
 
 Define_Module(valuePropagation_ApplicationModule);
 
 void valuePropagation_ApplicationModule::startup() {
-    tempTreshold = par("tempTreshold");
-    totalPackets = 0;
-    currMaxReceivedValue = -1.0;
-    currMaxSensedValue = -1.0;
-    sentOnce = 0;
-    theValue = 0;
-    sensedValues.clear();
-    setTimer(REQUEST_SAMPLE,0);
+	tempTreshold = par("tempTreshold");
+	totalPackets = 0;
+	currMaxReceivedValue = -1.0;
+	currMaxSensedValue = -1.0;
+	sentOnce = 0;
+	theValue = 0;
+	setTimer(REQUEST_SAMPLE,0);
 }
 
 void valuePropagation_ApplicationModule::timerFiredCallback(int index) {
-    switch (index) {
-	case REQUEST_SAMPLE: {
-	    requestSensorReading();
-	    break;
+	switch (index) {
+		case REQUEST_SAMPLE: {
+			requestSensorReading();
+			break;
+		}
 	}
-    }
 }
 
 void valuePropagation_ApplicationModule::fromNetworkLayer(ApplicationGenericDataPacket* rcvPacket, const char *source, double rssi, double lqi) {
-    double receivedData = rcvPacket->getData();
-    // int sequenceNumber = rcvPacket->getSequenceNumber();
+	double receivedData = rcvPacket->getData();
 
-    totalPackets++;
-    if(receivedData > currMaxReceivedValue) currMaxReceivedValue = receivedData;
+	totalPackets++;
+	if(receivedData > currMaxReceivedValue) currMaxReceivedValue = receivedData;
 
-    if (receivedData > tempTreshold && !sentOnce) {
-	theValue = receivedData;
-	toNetworkLayer(createGenericDataPacket(receivedData,1),BROADCAST_NETWORK_ADDRESS);
-	sentOnce = 1;
-	trace() << "Got the value: " << theValue;
-    }
+	if (receivedData > tempTreshold && !sentOnce) {
+		theValue = receivedData;
+		toNetworkLayer(createGenericDataPacket(receivedData,1),BROADCAST_NETWORK_ADDRESS);
+		sentOnce = 1;
+		trace() << "Got the value: " << theValue;
+	}
 }
 
 void valuePropagation_ApplicationModule::handleSensorReading(SensorReadingGenericMessage *rcvReading) {
-    // int sensIndex =  rcvReading->getSensorIndex();
-    // string sensType(rcvReading->getSensorType());
-    double sensedValue = rcvReading->getSensedValue();
-    
-    if(sensedValue > currMaxSensedValue) currMaxSensedValue = sensedValue;
+	double sensedValue = rcvReading->getSensedValue();
 
-    if(sensedValue > tempTreshold && !sentOnce) {
-	theValue = sensedValue;
-	toNetworkLayer(createGenericDataPacket(sensedValue,1),BROADCAST_NETWORK_ADDRESS);
-	sentOnce = 1;
-    }
-    sensedValues.push_back(sensedValue);
+	if(sensedValue > currMaxSensedValue) currMaxSensedValue = sensedValue;
+
+	if(sensedValue > tempTreshold && !sentOnce) {
+		theValue = sensedValue;
+		toNetworkLayer(createGenericDataPacket(sensedValue,1),BROADCAST_NETWORK_ADDRESS);
+		sentOnce = 1;
+	}
 }
 
 void valuePropagation_ApplicationModule::finishSpecific() {
-    // output the value that the node has when the simulation stopped
-    EV <<  "Node [" << self << "] Value: " << theValue << "\n";
-
-    sensedValues.clear();
+    declareOutput("Value");
+    collectOutput("Value", "Celcius", theValue);
 }

65871db1a73c5f5c6086e24e703f17bc48605ee1
Yuriy Tselishchev <yuri.tse@gmail.com>
2010-08-04 07:19:46 +0000

    Renewed file headers

--- Castalia/src/Node/Communication/Radio/RadioSupportFunctions.cc
+++ Castalia/src/Node/Communication/Radio/RadioSupportFunctions.cc
@@ -1,12 +1,13 @@
 /****************************************************************************
- *  Copyright: National ICT Australia,  2007 - 2010							*
- *  Developed at the ATP lab, Networked Systems theme						*
+ *  Copyright: National ICT Australia,  2007 - 2010                         *
+ *  Developed at the Networks and Pervasive Computing program               *
  *  Author(s): Athanassios Boulis, Dimos Pediaditakis, Yuriy Tselishchev	*
- *  This file is distributed under the terms in the attached LICENSE file.	*
- *  If you do not find this file, copies can be found by writing to:		*
- *																			*
- *      NICTA, Locked Bag 9013, Alexandria, NSW 1435, Australia				*
- *      Attention:  License Inquiry.										*
+ *  This file is distributed under the terms in the attached LICENSE file.  *
+ *  If you do not find this file, copies can be found by writing to:        *
+ *                                                                          *
+ *      NICTA, Locked Bag 9013, Alexandria, NSW 1435, Australia             *
+ *      Attention:  License Inquiry.                                        *
+ *                                                                          *  
  ****************************************************************************/
 
 #include "RadioSupportFunctions.h"
--- Castalia/src/Wireless_Channel/WirelessChannel.cc
+++ Castalia/src/Wireless_Channel/WirelessChannel.cc
@@ -1,13 +1,14 @@
-/***************************************************************************
- *  Copyright: National ICT Australia,  2007 - 2010
- *  Developed at the ATP lab, Networked Systems theme
- *  Author(s): Athanassios Boulis, Yuriy Tselishchev
- *  This file is distributed under the terms in the attached LICENSE file.
- *  If you do not find this file, copies can be found by writing to:
- *
- *      NICTA, Locked Bag 9013, Alexandria, NSW 1435, Australia
- *      Attention:  License Inquiry.
- ***************************************************************************/
+/****************************************************************************
+ *  Copyright: National ICT Australia,  2007 - 2010                         *
+ *  Developed at the Networks and Pervasive Computing program               *
+ *  Author(s): Athanassios Boulis, Yuriy Tselishchev                        *
+ *  This file is distributed under the terms in the attached LICENSE file.  *
+ *  If you do not find this file, copies can be found by writing to:        *
+ *                                                                          *
+ *      NICTA, Locked Bag 9013, Alexandria, NSW 1435, Australia             *
+ *      Attention:  License Inquiry.                                        *
+ *                                                                          *  
+ ****************************************************************************/
 
 #include "WirelessChannel.h"
 
